a0931544525d9f28ba9f6f611b1e0ac1e12cee35
icekidtech <princeklvn7568@gmail.com>
2024-11-25 12:17:15 +0100

    New Commit

--- banner/models.py
+++ banner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Banner(models.Model):
+    name = models.CharField(max_length=100)
+    handle = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='uploads/')
+    
+    def __str__(self):
+        return self.name
